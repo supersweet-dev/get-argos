f734ab5afbe2cb82d24962d6f386090bcaf2fa81
supersweet-dev <pentagr4mi@gmail.com>
2022-10-04 15:12:35 -0500

    im

--- get-argos.sh
+++ get-argos.sh
@@ -4,7 +4,7 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt install --y git
 sudo apt-get install --y cmake libfreeimage-dev libfreeimageplus-dev qt5-default freeglut3-dev libxi-dev libxmu-dev liblua5.3-dev lua5.3 doxygen graphviz libgraphviz-dev asciidoc  qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools build-essentials
-wget "https://drive.google.com/uc?export=download&id=1oO2lb2LuLq4IrZmNMiJurWTotHp_pDye" argos3_simulator-3.0.0-x86_64-beta59.deb
+wget -O argos3_simulator-3.0.0-x86_64-beta59.deb "https://drive.google.com/uc?export=download&id=1oO2lb2LuLq4IrZmNMiJurWTotHp_pDye"
 sudo apt install ./argos3_simulator-3.0.0-x86_64-beta59.deb
 git clone https://github.com/lukey11-zz/Foraging-Swarm-Robot-ARGoS
 cd Foraging-Swarm-Robot-ARGoS/CPFA
