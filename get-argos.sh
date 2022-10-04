3574f95321d83374e976fcd2121ff46bf387e06b
supersweet-dev <jim@supersweet.dev>
2022-10-04 18:05:37 -0500

    switch example library

--- get-argos.sh
+++ get-argos.sh
@@ -40,19 +40,26 @@ InstallArgos() {
       sudo apt-get update
       sudo apt-get --yes --force-yes upgrade
       sudo apt --yes --force-yes install git build-essential
-      sudo apt-get --yes --force-yes install cmake libfreeimage-dev libfreeimageplus-dev freeglut3-dev libxi-dev libxmu-dev liblua5.3-dev lua5.3 doxygen graphviz libgraphviz-dev asciidoc qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools
+      sudo apt-get --yes --force-yes install cmake libfreeimage-dev libfreeimageplus-dev freeglut3-dev libxi-dev libxmu-dev liblua5.3-dev lua5.3 doxygen graphviz libgraphviz-dev asciidoc qtbase5-dev qtchooser qt5-qmake qtbase5-dev-tools google-perftools libgoogle-perftools4
+      echo "/usr/local/lib" | sudo tee -a /etc/ld.so.conf
+      sudo ldconfig
+      export PKG_CONFIG_PATH=/usr/local/lib/pkgconfig/ >>~/.bashrc
+      source ~/.bashrc
    fi
+
    git clone https://github.com/ilpincy/argos3.git argos3
    cd argos3 && mkdir build_simulator && cd build_simulator
-   cmake ../src && sudo make install && make doc
+   cmake ../src && make && make doc
+   sudo make install
    cd ..
-   git clone https://github.com/lukey11-zz/Foraging-Swarm-Robot-ARGoS Foraging-Swarm-Robot-ARGoS
-   cd Foraging-Swarm-Robot-ARGoS/CPFA
-   echo "/usr/local/lib" | sudo tee -a /etc/ld.so.conf
-   sudo ldconfig
-   ./build.sh
-   argos3 -c experiments/Random_CPFA_r104_tag1024_20by20.xml
+   git clone https://github.com/ilpincy/argos3-examples.git argos3-examples
+   cd argos3-examples
+   mkdir build && cd build
+   cmake -DCMAKE_BUILD_TYPE=Debug .. && make
+   cd ..
+   argos3 -c experiments/diffusion_10.argos
 }
+
 while getopts ":cwih" option; do
    case $option in
    c) # nuke everything cept x11-apps
