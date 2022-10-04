209bc14fceee6c5aa0e8cb20337c48d4af87e181
supersweet-dev <pentagr4mi@gmail.com>
2022-10-04 16:30:08 -0500

    oh and another thing

--- get-argos.sh
+++ get-argos.sh
@@ -26,3 +26,16 @@ while getopts ":h" option; do
          exit;;
    esac
 done
+Help()
+{
+   # Display Help
+   echo "Add description of the script functions here."
+   echo
+   echo "Syntax: scriptTemplate [-g|h|t|v|V]"
+   echo "options:"
+   echo "g     Print the GPL license notification."
+   echo "h     Print this Help."
+   echo "v     Verbose mode."
+   echo "V     Print software version and exit."
+   echo
+}
